df2122e818cfdac49b8b4c75d79c408893015bf1
muhammad waris <mwaris390@gmail.com>
2023-08-01 17:50:32 +0500

    new files add

---
+++ Backend/routes/authenticateToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+
+async function authenticateToken(req, res, next) {
+  const token = req.body.jwt;
+  if (!token){
+    console.log('Access denied. No token provided.');
+  }else{
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err){
+          console.log('Invalid token.');
+      }else{
+        if(user.id === req.body.uid){
+          next();
+        }
+      }
+    });
+  }
+  
+}
+
+module.exports = authenticateToken;
